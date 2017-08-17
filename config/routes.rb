d98966c63b61cc077d5e30afa9f89128e696b867
muramatsu-natsushi <natsushi@localhost.localdomain>
2017-08-17 14:48:04 +0000

    サインイン、サインアウトの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
   root  'static_pages#home'
 
