ad7468cd3fd7c5799d1994822f6ddbb8a672cba8
muramatsu-natsushi <natsushi@localhost.localdomain>
2017-08-25 12:05:03 +0000

    マイクロポストの表示機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @micropost = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
