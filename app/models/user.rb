e78d3bcec389aa4e7770e3ebc5e963a70a812fd9
muramatsu-natsushi <natsushi@localhost.localdomain>
2017-08-30 13:37:16 +0000

    フィード機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  def feed
+  Micropost.where("user_id = ?", id)
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name,  presence: true, length: { maximum: 50 }
